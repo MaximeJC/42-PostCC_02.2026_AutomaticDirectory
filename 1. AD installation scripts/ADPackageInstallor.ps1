0348140276202108ed624c4a80738d47b84f008d
MaximeJC <mgouraud@student.42lehavre.fr>
2026-02-18 16:59:39 +0100

    Add error maanagement

--- 1. AD installation scripts/ADPackageInstallor.ps1
+++ 1. AD installation scripts/ADPackageInstallor.ps1
@@ -4,8 +4,22 @@
 #
 # ----------------------------------------------------------------
 
-# Install AD DS and dependencies
-Install-WindowsFeature -name AD-Domain-Services -IncludeManagementTools
+Try {
+    # Install AD DS and dependencies
+    Install-WindowsFeature -name AD-Domain-Services -IncludeManagementTools
 
-# Install DNS and dependencies
-Install-WindowsFeature -Name DNS -IncludeManagementTools
+    # Install DNS and dependencies
+    Install-WindowsFeature -Name DNS -IncludeManagementTools
+}
+Catch {
+    Write-Host "Error message:" -ForegroundColor Red
+    Write-Host $_.Exception.Message
+
+    Write-Host "`nFull exception details:" -ForegroundColor Yellow
+    Write-Host $_.Exception.ToString()
+
+    Write-Host "`nStack trace:"
+    Write-Host $_.ScriptStackTrace
+}
+
+
