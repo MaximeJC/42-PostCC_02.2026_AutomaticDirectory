6e32ed355802beaea498e594c480d2d2cb390976
MaximeJC <mgouraud@student.42lehavre.fr>
2026-02-18 15:52:29 +0100

    AD DS and DNS install script

--- 1. AD installation scripts/ADPackageInstallor.ps1
+++ 1. AD installation scripts/ADPackageInstallor.ps1
@@ -3,3 +3,9 @@
 #                     ADPackageInstallor.ps1
 #
 # ----------------------------------------------------------------
+
+# Install AD DS and dependencies
+Install-WindowsFeature -name AD-Domain-Services -IncludeManagementTools
+
+# Install DNS and dependencies
+Install-WindowsFeature -Name DNS -IncludeManagementTools
