4492effcc9fb69420a0377b692004cbe9f11afda
unknown <Administrator@DOMOLIA.LAN>
2026-02-18 07:38:49 -0800

    Groups: ImportGroup.ps1

---
+++ Groups/ImportGroup.ps1
@@ -0,0 +1,25 @@
+using assembly System.Windows.Forms
+using assembly System.Drawing
+using module "..\Modules\Form.psm1"
+
+$prg_name = "RemoveUserToGroup"
+
+$argsData = @(
+    [Argument]::new("Origin group name", $true, "")
+    [Argument]::new("Destination group name", $true, "")
+)
+
+try {
+    $form = [CustomForm]::new($prg_name, $argsData)
+    $form.askInput()
+
+    $origin = $form.getFormValue("Origin group name")
+    $dest = $form.getFormValue("Destination group name")
+
+    Get-ADGroupMember -Identity $origin | ForEach-Object {Add-ADGroupMember -Identity $dest -Members $_.distinguishedName}
+}
+catch {
+    Write-Host "$_"
+    Show-ErrorMessage("$_")
+    exit(1);
+}
