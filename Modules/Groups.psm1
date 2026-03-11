fe3390df1e67d4e831b4d1ba5e35990484a35c11
Maxime <mgouraud@student.42lehavre.fr>
2026-03-11 14:03:24 +0100

    Fix: Fetch all properties for group sample in CheckGroupProperty

--- Modules/Groups.psm1
+++ Modules/Groups.psm1
@@ -1,5 +1,5 @@
 function CheckGroupProperty([string]$property) {
-    $sample = Get-ADGroup -Filter * -ResultSetSize 1 # get a group sample
+    $sample = Get-ADGroup -Filter * -ResultSetSize 1 -Properties * # get a group sample
     $availableProperties = $sample | Get-Member -MemberType Properties | Select-Object -ExpandProperty Name  ## get all fields
     if ($availableProperties -notcontains $property) {
         throw "The property '$property' doesn't exists in groups"
